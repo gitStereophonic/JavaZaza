27553f6b2a08229affad83d8673971a6cf9d9422
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-02-22 14:02:15 +0300

    [DONE][BLOCK1] Hello world task

---
+++ HelloWorld/src/app/HelloWorld.java
@@ -0,0 +1,12 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        // Console output
+        System.out.println("Hello, World!");
+        // Widget message
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+    }
+}
